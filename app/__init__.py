d93d38e4d37291629e7401b4789f1baf34d9e8ca
viiamie <71726874+viiamie@users.noreply.github.com>
2022-05-10 19:37:20 -0400

    login/registration/user management

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,60 @@
-"""A simple flask web app"""
 import os
-from flask import Flask, render_template
+import flask_login
+from flask import Flask
 from flask_bootstrap import Bootstrap5
+from flask_wtf.csrf import CSRFProtect
+
+from app.auth import auth
+from app.auth import auth
 from app.cli import create_database
+from app.context_processors import utility_text_processors
 from app.db import db
 from app.db.models import User
-from app import simple_pages
+from app.error_handlers import error_handlers
+from app.simple_pages import simple_pages
+from app.db import database
+from flask_cors import CORS
 
+login_manager = flask_login.LoginManager()
 
-def page_not_found(e):
-    return render_template("404.html"), 404
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
+    if  os.environ.get("FLASK_ENV") == "production":
+        app.config.from_object("app.config.ProductionConfig")
+    elif os.environ.get("FLASK_ENV") == "development":
+        app.config.from_object("app.config.DevelopmentConfig")
+    elif os.environ.get("FLASK_ENV") == "testing":
+        app.config.from_object("app.config.TestingConfig")
+        app.config['WTF_CSRF_ENABLED'] = False
+
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+    # Needed for CSRF protection of form submissions and WTF Forms
+    csrf = CSRFProtect(app)
     bootstrap = Bootstrap5(app)
-    app.register_blueprint(simple_pages.simple_pages)
-    app.register_error_handler(404, page_not_found)
-    db_dir = "database/db.sqlite"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
+    # these load functions with web interface
+    app.register_blueprint(simple_pages)
+    app.register_blueprint(auth)
+    app.register_blueprint(database)
+    # these load functionality without a web interface
+    app.register_blueprint(error_handlers)
+    app.context_processor(utility_text_processors)
     # add command function to cli commands
     app.cli.add_command(create_database)
-
+    db.init_app(app)
+    api_v1_cors_config = {
+    "methods": ["OPTIONS", "GET", "POST"],
+    }
+    CORS(app, resources={"/api/*": api_v1_cors_config})
+    # Run once at startup:
     return app
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except:
+        return None
--- app/cli/__init__.py
+++ app/cli/__init__.py
@@ -1,3 +1,4 @@
+import os
 import click
 from flask.cli import with_appcontext
 from app.db import db
@@ -6,4 +7,8 @@ from app.db import db
 @click.command(name='create-db')
 @with_appcontext
 def create_database():
+    root = os.path.dirname(os.path.abspath(__file__))
+    dbdir = os.path.join(root, '../../database')
+    if not os.path.exists(dbdir):
+        os.mkdir(dbdir)
     db.create_all()
